32b875a94033fc0536cfd8c12441a8d5a80a1442
Lungu Stefan <lungustefan24@yahoo.com>
2022-11-06 11:49:40 +0200

    Implementing random colors and read data from file

--- Lungu Stefan Andrei-triunghi/Program.cs
+++ Lungu Stefan Andrei-triunghi/Program.cs
@@ -14,11 +14,33 @@ namespace Lungu_Stefan_Andrei_triunghi
 {
     class SimpleWindow : GameWindow
     {
+        List<List<float>> coord = new List<List<float>>();
+        List<Color> colors = new List<Color>();
+
         public SimpleWindow() : base(800, 600)
         {
             KeyDown += Keyboard_KeyDown;
+            Console.WriteLine(" W - muta sus ");
+            Console.WriteLine(" S - muta jos");
+            Console.WriteLine(" A - muta dreapta");
+            Console.WriteLine(" D - muta stanga");
+            Console.WriteLine(" Click dreapta mouse - rotatie clockwise");
+            Console.WriteLine(" Click stanga mouse - rotatie counter-clockwise");
+
+            coord=GetCoordonate();
+            setColors();
         }
 
+        //functie ce genereaza culori random, din interval RGB
+        void setColors()
+        {
+            var rnd = new Random();
+            colors.Clear();
+            for(int i=0; i<4; i++)
+            {
+                colors.Add(Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256)));
+            }
+        }
 
         void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
         {
@@ -31,11 +53,11 @@ namespace Lungu_Stefan_Andrei_triunghi
                     this.WindowState = WindowState.Fullscreen;
             if (e.Key == Key.W)
             {
-                GL.Scale(1.1, 1.1, 1.1);
+                GL.Translate(0.0,0.1,0);
             }
             if (e.Key == Key.S)
             {
-                GL.Scale(0.9, 0.9, 0.9);
+                GL.Translate(0.0, -0.1, 0);
             }
             if (e.Key == Key.D)
             {
@@ -45,6 +67,12 @@ namespace Lungu_Stefan_Andrei_triunghi
             {
                 GL.Translate(-0.1, 0, 0);
             }
+
+            if(e.Key==Key.C)
+            {
+                setColors();
+            }
+
 
         }
 
@@ -56,22 +84,25 @@ namespace Lungu_Stefan_Andrei_triunghi
 
             if (mouse[OpenTK.Input.MouseButton.Right])
             {
-                GL.Rotate(3, 0, 1, 0);
+                GL.Rotate(-3.0f,0.0f,0.0f,1.0f);
             }
             if (mouse[OpenTK.Input.MouseButton.Left])
             {
-                GL.Rotate(-3, 0, 1, 0);
+                GL.Rotate(3.0f, 0.0f, 0.0f, 1.0f);
             }
+
 
         }
 
         protected override void OnLoad(EventArgs e)
         {
-            GL.ClearColor(Color.SteelBlue);
+            
+            GL.ClearColor(Color.DarkBlue);
         }
 
         protected override void OnResize(EventArgs e)
         {
+            base.OnResize(e);
             GL.Viewport(0, 0, Width, Height);
 
             GL.MatrixMode(MatrixMode.Modelview);
@@ -81,21 +112,40 @@ namespace Lungu_Stefan_Andrei_triunghi
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
+            base.OnRenderFrame(e);
             GL.Clear(ClearBufferMask.ColorBufferBit);
+            DrawFigure();
+            this.SwapBuffers();
+        }
+        public List<List<float>> GetCoordonate()
+        {
+            //O lista de float-uri reprezinta coordonatele unui vertex
+            //Coordonatele sunt cate 3 pe un rand, delimitate prin ","
 
-            GL.Begin(PrimitiveType.Triangles);
+            List<List<float>> list = new List<List<float>>();
+            foreach (string line in System.IO.File.ReadLines("coordonate.txt"))
+            {
+                list.Add(line.Split(',')?.Select(float.Parse)?.ToList());
+            }
 
-            GL.Color3(Color.Red);
-            GL.Vertex2(0.0f, 1.0f);
-            GL.Color3(Color.SpringGreen);
-            GL.Vertex2(-1.0f, -1.0f);
-            GL.Color3(Color.Ivory);
-            GL.Vertex2(1.0f, -1.0f);
+          
+
+            return list;
+        }
+        private void DrawFigure()
+        {
+            GL.Begin(PrimitiveType.Polygon);
+
+            GL.Color3(colors[0]);
+            GL.Vertex2(coord[0][0], coord[0][1]);
+            GL.Color3(colors[1]);
+            GL.Vertex2(coord[1][0], coord[1][1]);
+            GL.Color3(colors[2]);
+            GL.Vertex2(coord[2][0], coord[2][1]);
+            GL.Color3(colors[3]);
+            GL.Vertex2(coord[3][0], coord[3][1]);
 
             GL.End();
-
-
-            this.SwapBuffers();
         }
 
         [STAThread]
